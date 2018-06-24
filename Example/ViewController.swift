9932a8ba13dfd843e49ac8945e46764c0b8e9f91
Petr Zvoníček <zvonicek@gmail.com>
2018-06-24 18:06:56 +0200

    Update example project

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -11,30 +11,34 @@ import CellKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet var tableView: UITableView!
-    var dataSource: EquatableCellModelDataSource!
+    @IBOutlet private var tableView: UITableView!
+    private var dataSource: EquatableCellModelDataSource!
 
-    var phones = ["iPhone", "iPhone 3G", "iPhone 3GS", "iPhone 4", "iPhone 4S", "iPhone 5", "iPhone 5s", "iPhone 6", "iPhone 6s", "iPhone SE", "iPhone 7", "iPhone 8", "iPhone X"]
+    private var phones = ["iPhone", "iPhone 3G", "iPhone 3GS", "iPhone 4", "iPhone 4S", "iPhone 5", "iPhone 5s", "iPhone 6", "iPhone 6s", "iPhone SE", "iPhone 7", "iPhone 8", "iPhone X"]
+
+    private var phonesSection: EquatableCellModelSection {
+        let cellModels = phones.map { DeviceiOSCellModel(name: $0) }
+        return EquatableCellModelSection(cells: cellModels, identifier: "Section 1")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        let cellModels = phones.map { DeviceiOSCellModel(name: $0) }
-        let section = EquatableCellModelSection(cells: cellModels, identifier: "Section 1")
-        dataSource = EquatableCellModelDataSource(tableView, sections: [section])
+        dataSource = EquatableCellModelDataSource(tableView, sections: [phonesSection])
         dataSource.delegate = self
         tableView.dataSource = dataSource
         tableView.delegate = dataSource
     }
 
-    @IBAction func didTapAddSection() {
-        
+    @IBAction func didTapReset() {
+        dataSource?.sections = [phonesSection]
     }
 
     @IBAction func didTapAddRowA() {
         if let section = dataSource?.sections[0] {
             var section = section
-            section.cells.insert(DeviceiOSCellModel(name: "iPhone X \(arc4random() % 100)"), at: 0)
+            let phoneName = "iPhone \(arc4random() % 100)"
+            section.cells.insert(DeviceiOSCellModel(name: phoneName), at: 0)
             dataSource?.sections = [section]
         }
     }
