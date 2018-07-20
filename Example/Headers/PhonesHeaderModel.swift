7760adb89f3d53e9274e9ac7c5f9e362107db576
Petr Zvoníček <zvonicek@gmail.com>
2018-07-20 13:57:08 +0200

    Add header to example project

---
+++ Example/Headers/PhonesHeaderModel.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewHeaderViewModel.swift
+//  SmallAlarmClient
+//
+//  Created by Adam Leitgeb on 23/04/2018.
+//  Copyright © 2018 Adam Leitgeb. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIView
+import CellKit
+
+struct PhonesHeaderModel {
+    let title: String
+}
+
+extension PhonesHeaderModel: SupplementaryViewModel, CellConvertible {
+    typealias Cell = PhonesHeader
+
+    var height: CGFloat {
+        return 50
+    }
+}
