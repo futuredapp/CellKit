342d7b4fa7e5669db1db2c1e8ec5fd772d98d8ba
Matěj Kašpar Jirásek <matej.jirasek@me.com>
2019-07-09 13:38:08 +0200

    Add platforms to package

--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "CellKit",
+    platforms: [.iOS(.v8), .tvOS(.v9)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
